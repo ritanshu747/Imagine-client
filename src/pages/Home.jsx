d72eada2a92bf7b15513d71fb3d9b108af2de0a1
ritanshu747 <73697511+ritanshu747@users.noreply.github.com>
2024-02-28 16:00:49 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,114 @@
+import { useState, useEffect } from "react";
+import { Loader, FormField, Card } from "../components";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data?.map((post) => <Card key={post._id} {...post} />);
+  }
+  return (
+    <h2 className="mt-5 font-bold text-[#4634a9] text-xl uppercase">{title}</h2>
+  );
+};
+
+const Home = () => {
+  const [posts, setPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://imagine-server.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.ok) {
+          const result = await response.json();
+          setPosts(result.data.reverse());
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = posts.filter(
+          (post) =>
+            post.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            post.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setSearchResults(searchResults);
+      }, 500)
+    );
+  };
+
+  return (
+    <section>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          IMAGIN-AI is a new AI system that can create realistic images and art
+          from a description in natural language.
+        </p>
+      </div>
+      <div className="mt-16">
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search Posts by name or prompt"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
+      </div>
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-col-4 sm:grid-col-3 xs:grid-cols-2 grid-col-1 gap-3">
+              {searchText ? (
+                <RenderCards
+                  data={searchResults}
+                  title="No search results found"
+                />
+              ) : (
+                <RenderCards data={posts} title="No posts found" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
